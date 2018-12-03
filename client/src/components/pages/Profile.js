ec70dc1bd21824dc27f1f09bc0341e91e83c87e5
Anna Dorsch <a.dorsch@ymail.com>
2018-12-03 18:00:37 +0100

    merging

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,12 +1,7 @@
 // import React from 'react';
 import React from "react";
-<<<<<<< HEAD
 // import { NavLink, Route, Switch } from "react-router-dom";
 import { Button, Col, FormGroup, Row, Input, Form, Label } from "reactstrap";
-=======
-import { NavLink, Route, Switch } from "react-router-dom";
-import { Button, Col, FormGroup, ListGroupItem, Row, Input,Form,Label, } from "reactstrap";
->>>>>>> 83d2f39d57c66a1929d44b3414193244926aa0d2
 import api from "../../api";
 // import axios from "axios";
 
@@ -61,65 +56,47 @@ class Profile extends React.Component {
           <Col sm={5} className="col-text">
             <h2>hi {this.state.username}</h2>
             <Form>
-<<<<<<< HEAD
               <Row>
                 {this.state.URL !== "" && (
-                  <img src={this.state.URL} style={{ height: 200 }} />
+                  <img
+                    src={this.state.URL}
+                    style={{ height: 150, width: 150 }}
+                  />
                 )}
                 {this.state.message && (
                   <div className="info">{this.state.message}</div>
                 )}
               </Row>
-              <Row sm={7}>Username: {this.state.username} </Row>
-              <Row sm={7}>Email: {} </Row>
-              {/* <Row sm={7}>Password: {this.state.password} </Row> */}
-              <FormGroup check inline>
-                <Label check>
-                  <Input type="checkbox" /> Diver
-                </Label>
-              </FormGroup>
-              <FormGroup check inline>
-                <Label check>
-                  <Input type="checkbox" /> Surfer
-                </Label>
-              </FormGroup>
+              <Row sm={7}>Username:{this.state.username} </Row>
+              <Row sm={7}>Email: {this.state.email} </Row>
+              <Row sm={7}> </Row>
+              <FormGroup check inline />
               <Row sm={7}>
-                {/*  change this to FORM if something weird happens */}
-                <FormGroup onSubmit={e => this.handleSubmit(e)}>
-                  <Input type="file" onChange={e => this.handleChange(e)} />
-                  <Button type="submit">Upload</Button>
-                </FormGroup>
+                <Form onSubmit={e => this.handleSubmit(e)}>
+                  <Input type="file" onChange={e => this.handleChange(e)} />{" "}
+                  <Button size="sm" outline color="info" type="submit">
+                    Upload
+                  </Button>
+                </Form>
               </Row>
               <Row sm={7}>
-                <Button onClick={e => this.handleClick(e)}>Edit</Button>
-                <Button onClick={e => this.handleClick(e)}>Delete</Button>
+                <Button
+                  size="sm"
+                  outline
+                  color="info"
+                  onClick={e => this.handleClick(e)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  size="sm"
+                  outline
+                  color="info"
+                  onClick={e => this.handleClick(e)}
+                >
+                  Delete
+                </Button>
               </Row>
-=======
-            <Row>
-            {this.state.URL!=="" && <img src={this.state.URL} style={{height: 150, width: 150}} />}
-            {this.state.message && <div className="info"> 
-            {this.state.message}
-            </div>}
-            </Row>
-            <Row sm={7}>Username:{this.state.username} </Row>
-            <Row sm={7}>Email: {this.state.email} </Row>
-            <Row sm={7}> </Row>
-            <FormGroup check inline>
-          
-        </FormGroup>
-            <Row sm={7}>
-              <Form onSubmit={e => this.handleSubmit(e)}>
-              <Input type="file" onChange={e => this.handleChange(e)} />{" "}
-              <Button size="sm" outline color="info" type="submit">Upload</Button></Form>
-
-            </Row>
-            <Row sm={7}>
-              <Button size="sm" outline color="info" onClick={e => this.handleClick(e)}>Edit</Button>
-              <Button size="sm" outline color="info" onClick={e => this.handleClick(e)}>Delete</Button>
-            </Row>    
-                
-            
->>>>>>> 83d2f39d57c66a1929d44b3414193244926aa0d2
             </Form>
           </Col>
         </Row>
--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -23,18 +23,11 @@ class Signup extends Component {
     e.preventDefault();
     let data = {
       username: this.state.username,
-<<<<<<< HEAD
-      email: this.state.email,
-      password: this.state.password
+      password: this.state.password,
+      email: this.state.email
     };
     api
       .signup(data)
-=======
-      password: this.state.password,
-      email: this.state.email
-    }
-    api.signup(data)
->>>>>>> 83d2f39d57c66a1929d44b3414193244926aa0d2
       .then(result => {
         console.log("SUCCESS!");
         this.props.history.push("/"); // Redirect to the home page
@@ -47,74 +40,60 @@ class Signup extends Component {
       <div className="Signup">
         <h2>Signup</h2>
         <Form>
-<<<<<<< HEAD
           <FormGroup row>
-            <Label for="exampleUsername" sm={1}>
+            <Label for="exampleText" sm={1}>
               Username:{" "}
             </Label>
-            <Col sm={4}>
+            <Col sm={2}>
               <Input
-                type="username"
+                type="text"
                 value={this.state.username}
                 onChange={e => this.handleInputChange("username", e)}
                 name="username"
-                id="exampleusername"
+                id="exampleUsername"
                 placeholder="crazyDiver666"
               />
             </Col>
           </FormGroup>
           <FormGroup row>
             <Label for="examplePassword" sm={1}>
-              Password :
+              Password
             </Label>
-            <Col sm={4}>
+            <Col sm={2}>
               <Input
                 type="password"
+                value={this.state.password}
+                onChange={e => this.handleInputChange("password", e)}
                 name="password"
                 id="examplePassword"
-                placeholder="enter Password"
+                placeholder="your secret"
               />
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label for="examplePassword" sm={1}>
+            <Label for="exampleEmail" sm={1}>
               Email:
             </Label>
-            <Col sm={4}>
+            <Col sm={2}>
               <Input
-                type="password"
-                value={this.state.username}
-                onChange={e => this.handleInputChange("username", e)}
-                name="password"
-                id="examplePassword"
+                type="email"
+                value={this.state.email}
+                onChange={e => this.handleInputChange("email", e)}
+                name="email"
+                id="exampleEmail"
                 placeholder="crazyDiver666@coralreef.com"
               />
             </Col>
 
-            <Button onClick={e => this.handleClick(e)}>Signup</Button>
+            <Button
+              size="sm"
+              outline
+              color="info"
+              onClick={e => this.handleClick(e)}
+            >
+              Signup
+            </Button>
           </FormGroup>
-=======
-        <FormGroup row>
-          <Label for="exampleText" sm={1}>Username: </Label>
-          <Col sm={2}>
-            <Input type="text" value= {this.state.username} onChange={(e) => this.handleInputChange("username", e)}name="username" id="exampleUsername" placeholder="crazyDiver666" />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="examplePassword" sm={1}>Password</Label>
-          <Col sm={2}>
-            <Input type="password"  value= {this.state.password} onChange={(e) => this.handleInputChange("password", e)} name="password" id="examplePassword" placeholder="your secret" />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="exampleEmail" sm={1}>Email:</Label>
-          <Col sm={2}>
-            <Input type="email" value={this.state.email} onChange={(e) => this.handleInputChange("email", e)}name="email" id="exampleEmail" placeholder="crazyDiver666@coralreef.com" />
-          </Col>
-          
-          <Button size="sm" outline color="info" onClick={(e) => this.handleClick(e)}>Signup</Button>
-        </FormGroup>
->>>>>>> 83d2f39d57c66a1929d44b3414193244926aa0d2
         </Form>
 
         {this.state.message && (
